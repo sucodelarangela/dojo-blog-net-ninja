d021185bd325b30d55109b0db9d833931389677d
Angela Caldas <aes.caldas@gmail.com>
2022-05-14 13:39:11 -0300

    Using CSS files and inline CSS

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Blog Dojo</h1>
+      <h1>Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create">Novo post</a>
@@ -11,3 +11,20 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+/*
+É possível utilizar CSS inline no JSX, mas ele funciona como um valor dinâmico, logo, deve ser adicionado entre {} para ser reconhecido como JSX e dentro de outro par de {} para ser reconhecido como objeto.
+
+Dessa forma, toda propriedade CSS que possui separação com "-" deve ser escrita com camelCase para não ser confundida com a sintaxe JavaScript.
+
+Exemplo:
+
+<a href="/create"
+  style={{
+    color: 'white',
+    backgroundColor: '#f1356d',
+    borderRadius: '8px'
+  }}
+>Novo post</a>
+
+*/
