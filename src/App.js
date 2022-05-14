f7590d4a2ad861fbbc907817bcc5ccb1b4e312cd
Angela Caldas <aes.caldas@gmail.com>
2022-05-14 13:29:23 -0300

    Nesting components in root component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 /*
 Root component.
@@ -7,21 +9,11 @@ Deve ser exportado para index.js
 */
 
 function App() {
-  const title = 'Bem vindo ao blog';
-  const likes = '50';
-  const link = 'https://www.google.com';
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Curtido por {likes} pessoas</p>
-        <p>{10}</p>
-        <p>{'Hello, ninjas'}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.round(Math.random() * 10)}</p>
-
-        <a href={link}>Google site</a>
+        <Home />
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Blog Dojo</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">Novo post</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
