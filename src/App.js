e2777fb81fbb99cc84d0e01a7722d9973194ae57
Angela Caldas <aes.caldas@gmail.com>
2022-05-14 13:20:34 -0300

    Testing and understanding dynamic data in JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+/*
+Root component.
+Também pode ser uma arrow function.
+Deve ser exportado para index.js
+*/
+
 function App() {
+  const title = 'Bem vindo ao blog';
+  const likes = '50';
+  const link = 'https://www.google.com';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Curtido por {likes} pessoas</p>
+        <p>{10}</p>
+        <p>{'Hello, ninjas'}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.round(Math.random() * 10)}</p>
+
+        <a href={link}>Google site</a>
+      </div>
     </div>
   );
 }
