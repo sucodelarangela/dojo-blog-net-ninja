61ed4a94715fedcef942f0d0f497897202ffbfcd
Angela Caldas <aes.caldas@gmail.com>
2022-05-15 19:22:14 -0300

    Getting to know React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; //importing destructured react router
 
 /*
 Root component.
@@ -9,12 +10,18 @@ Deve ser exportado para index.js
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
