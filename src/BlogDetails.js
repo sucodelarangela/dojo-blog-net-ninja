45f676414dd3e08c58fdc34cdb5ad6ce43dca571
Angela Caldas <aes.caldas@gmail.com>
2022-05-16 19:29:16 -0300

    Reusing custom hook useFetch to deal with routes

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,31 @@
 import {useParams} from 'react-router-dom';
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
   // Pega os parâmetros da url, similar ao express
   const {id} = useParams();
 
+  // usando o custom hook useFetch
+  const {
+    data: blog,
+    error,
+    isPending
+  } = useFetch('http://localhost:8000/blogs/' + id);
+
   return (
     <div className="blog-details">
-      <h2>Blog details - {id}</h2>
+      {/* Verificação: se o loading estiver pendente, adiciona a mensagem */}
+      {isPending && <div>Loading...</div>}
+      {/* Verificação: se houver um erro, mostra o erro em tela */}
+      {error && <div>{error}</div>}
+      {/* Verificação: se houver um post na API, cria o article e insere seu conteúdo */}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Escrito por {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
